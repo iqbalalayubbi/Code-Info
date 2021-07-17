f776e4be80f4a12ba38d0d23e4abf1e944251da9
iqbalalayubbi <79147766+iqbalalayubbi@users.noreply.github.com>
2021-07-18 06:43:08 +0800

    Update toward-position.gd

--- toward-position.gd
+++ toward-position.gd
@@ -1,5 +1,13 @@
+# toward position peluru
 var bullet_instance = bullet.instance()
 bullet_instance.position = $BulletPoint.get_global_position()
 bullet_instance.rotation_degrees = rotation_degrees
 bullet_instance.apply_impulse(Vector2(),Vector2(speedBullet,0).rotated(rotation))
 get_tree().get_root().add_child(bullet_instance)
+
+# enemy ai movement to sprite ( taro di func physic_process)
+
+var angle = get_angle_to(get_parent().get_node("player").position)
+velocity.x = cos(angle)
+velocity.y = sin(angle)
+global_position += velocity * 100 * delta
