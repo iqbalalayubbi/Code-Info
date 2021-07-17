ce326ac175bf9ea14482cf684630893004bf98c7
iqbalalayubbi <79147766+iqbalalayubbi@users.noreply.github.com>
2021-07-18 06:40:02 +0800

    Create toward-position.gd

---
+++ toward-position.gd
@@ -0,0 +1,5 @@
+var bullet_instance = bullet.instance()
+		bullet_instance.position = $BulletPoint.get_global_position()
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(),Vector2(speedBullet,0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
